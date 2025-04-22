76a952bbc9d4a5ed10549d0a8860b7f12b55d8a5
Romain Gremaud <rgremau@gmail.com>
2025-04-22 11:31:10 -0400

    Change names on initial function

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ require_relative 'lib/players'
 new_game = Gameboard.new
 new_game.rules
 
-game = Players.new('You', 'Me')
+game = Players.new('Joe', 'Sally')
 game.display_board
 game.current_players
 game.play_game
